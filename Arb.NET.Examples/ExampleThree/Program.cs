b8b317eb7a6ffdc76459d3b57e6319dee4a8f907
Michal-MK <michalhz159@gmail.com>
2026-02-18 21:18:00 +0100

    Prepare Nuget packaging

---
+++ Arb.NET.Examples/ExampleThree/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExampleThree.Localizations;
+
+Console.WriteLine("Hello, World!");
+
+AppLocale l = new(System.Globalization.CultureInfo.GetCultureInfo("en"));
+Console.WriteLine($"AppTitle       : {l.AppTitle}");
