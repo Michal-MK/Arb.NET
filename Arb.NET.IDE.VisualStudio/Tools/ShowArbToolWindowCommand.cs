24015283a79bca5b169af6dfa9ea3f417a37e5d9
Michal-MK <michalhz159@gmail.com>
2026-02-20 21:08:41 +0100

    Cleanup

--- Arb.NET.IDE.VisualStudio/Tools/ShowArbToolWindowCommand.cs
+++ Arb.NET.IDE.VisualStudio/Tools/ShowArbToolWindowCommand.cs
@@ -3,45 +3,47 @@ using System;
 using System.ComponentModel.Design;
 using System.Threading.Tasks;
 
-namespace Arb.NET.IDE.VisualStudio.Tools {
-    internal sealed class ShowArbToolWindowCommand {
-        public const int CommandId = 0x0100;
-        public static readonly Guid CommandSet = new Guid("d8c8b3e1-2f3c-4b6c-9d4e-1f2b3c4d5e6f");
+namespace Arb.NET.IDE.VisualStudio.Tools;
 
-        private readonly AsyncPackage package;
+internal sealed class ShowArbToolWindowCommand {
+    private const int COMMAND_ID = 0x0100;
+    // This GUID must match the one in the .vsct file for the command's parent menu/toolbar
+    private static readonly Guid COMMAND_SET = new("d8c8b3e1-2f3c-4b6c-9d4e-1f2b3c4d5e6f");
 
-        private ShowArbToolWindowCommand(AsyncPackage package, Microsoft.VisualStudio.Shell.OleMenuCommandService commandService) {
-            this.package = package;
+    private readonly AsyncPackage package;
 
-            var cmdId = new CommandID(CommandSet, CommandId);
-            var cmd = new OleMenuCommand(Execute, cmdId);
+    private ShowArbToolWindowCommand(AsyncPackage package, OleMenuCommandService commandService) {
+        this.package = package;
 
-            commandService.AddCommand(cmd);
+        CommandID cmdId = new(COMMAND_SET, COMMAND_ID);
+        OleMenuCommand cmd = new(Execute, cmdId);
+
+        commandService.AddCommand(cmd);
+    }
+
+    public static async Task InitializeAsync(AsyncPackage package) {
+        await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+        if (await package.GetServiceAsync(typeof(IMenuCommandService)) is not OleMenuCommandService commandService) {
+            throw new InvalidOperationException("Unable to get OleMenuCommandService.");
         }
 
-        public static async Task InitializeAsync(AsyncPackage package) {
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+        // This is apparently how VS does it?
+        // ReSharper disable once ObjectCreationAsStatement
+        new ShowArbToolWindowCommand(package, commandService);
+    }
 
-            var commandService = await package.GetServiceAsync(typeof(IMenuCommandService))
-                                   as Microsoft.VisualStudio.Shell.OleMenuCommandService;
+    private void Execute(object sender, EventArgs e) {
+        _ = package.JoinableTaskFactory.RunAsync(async () => {
+            ToolWindowPane window = await package.ShowToolWindowAsync(
+                typeof(ArbToolWindow),
+                0,
+                true,
+                package.DisposalToken);
 
-            if (commandService == null) {
-                throw new InvalidOperationException("Unable to get OleMenuCommandService.");
+            if (window == null) {
+                throw new InvalidOperationException("Failed to show Arb.NET tool window.");
             }
-
-            new ShowArbToolWindowCommand(package, commandService);
-        }
-
-        private void Execute(object sender, EventArgs e) {
-            ThreadHelper.ThrowIfNotOnUIThread();
-
-            package.JoinableTaskFactory.RunAsync(async () => {
-                var window = await package.ShowToolWindowAsync(
-                    typeof(ArbToolWindow),
-                    0,
-                    true,
-                    package.DisposalToken);
-            });
-        }
+        });
     }
 }
