e8818d6aa2df38a2ca672dc30c6c17ea211e4fd4
Michal-MK <michalhz159@gmail.com>
2026-02-23 22:03:55 +0100

    Add missing extension

---
+++ Arb.NET.Examples/ExampleFive/Extensions/ArbExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace ExampleFive.Extensions;
+
+[ContentProperty(nameof(Text))]
+public class ArbExtension : IMarkupExtension {
+    public string Text { get; set; }
+
+    public object ProvideValue(IServiceProvider serviceProvider) {
+        AppLocale locale = new(new CultureInfo("en"));
+        return locale.GetType().GetProperty(Text)?.GetValue(locale) ?? "<MISSING>";
+    }
+}
