cedc5cfce17b5622373aeefa2fc3e480322f9daa
Michal-MK <michalhz159@gmail.com>
2026-02-28 09:38:39 +0100

    Modernize the MAUI project Xaml stuff

---
+++ Arb.NET.Examples/ExampleFive/GlobalXmlns.cs
@@ -0,0 +1,6 @@
+﻿[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "ExampleFive.Extensions")]
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "ExampleFive")]
